d72e3a605f65f9ccb0728643e19a90a501c5d625
Mateus Pureza <mateus.pureza@eplugin.app.br>
2025-10-29 19:41:03 -0300

    Excluir webhooks do CSRF para permitir notificações do Mercado Pago

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Excluir webhook do CSRF para Mercado Pago poder notificar
+        $middleware->validateCsrfTokens(except: [
+            'webhooks/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
